13e7530067358064a9c9417723f8b14896043701
hugovallada <valladahugo@gmail.com>
2021-02-26 07:42:04 -0300

    Project Created

---
+++ src/main/java/com/github/hugovallada/orderscontrol/OrdersControlApplication.java
@@ -0,0 +1,13 @@
+package com.github.hugovallada.orderscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersControlApplication.class, args);
+	}
+
+}
