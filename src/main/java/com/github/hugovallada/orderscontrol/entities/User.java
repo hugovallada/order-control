b20f42a3d417466c696cfe216a2aa673709bbbe9
hugovallada <valladahugo@gmail.com>
2021-02-26 10:52:42 -0300

    Add JPA and H2 support

--- src/main/java/com/github/hugovallada/orderscontrol/entities/User.java
+++ src/main/java/com/github/hugovallada/orderscontrol/entities/User.java
@@ -2,6 +2,11 @@ package com.github.hugovallada.orderscontrol.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,9 +19,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class User implements Serializable {
 	
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
